eb2b132831cd74340442ceb586967af483b3d73e
Veronica Rizzi <vevarizzi@gmail.com>
2022-09-15 20:19:42 -0300

    idao deletado

--- ClinicaOdontologica/src/main/java/com/dh/clinicaodontologica/dao/IDao.java
+++
@@ -1,9 +0,0 @@
-package com.dh.clinicaodontologica.dao;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface IDao<T> {
-    public T salvar(T t) throws SQLException;
-    public List<T> buscarTodos();
-}
